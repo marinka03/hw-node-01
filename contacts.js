29ae8e8d6e2bf6a1d68e9cc89a493f5536e4e73b
Marina Chernyakina <chernyakinamarina@gmail.com>
2024-01-11 11:16:42 +0200

    add switch with other case for contacts, and fix function in contacts.js

--- contacts.js
+++ contacts.js
@@ -1,56 +1,42 @@
-const fs = require('fs').promises;
-const contactsPath = './db/contacts.json';
-
+const fs = require('fs/promises');
 const { randomId } = require('./helpers/createId');
-function listContacts() {
-  return fs
-    .readFile(contactsPath, { encoding: 'utf-8' })
-    .then(data => console.log(JSON.parse(data)))
-    .catch(err => console.log(err));
-}
 
-function getContactById(contactId) {
-  return fs
-    .readFile(contactsPath, { encoding: 'utf-8' })
-    .then(data => {
-      const formatData = JSON.parse(data);
-      const el = formatData.find(contact => contact.id === contactId);
-      return console.log(el !== undefined ? el : null);
-    })
-    .catch(err => console.log(err));
-}
+const path = require('path');
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
-function removeContact(contactId) {
-  return fs
-    .readFile(contactsPath, { encoding: 'utf-8' })
-    .then(data => {
-      const formatData = JSON.parse(data);
-      const deleteEl = formatData.find(item => item.id === contactId);
-      formatData.filter(el => el !== deleteEl);
-      return console.log(deleteEl !== undefined ? deleteEl : null);
-    })
-    .catch(err => console.log(err));
-}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, 'utf-8');
+  return await JSON.parse(data);
+};
 
-function addContact(name, email, phone) {
-  return fs
-    .readFile(contactsPath, { encoding: 'utf-8' })
-    .then(data => {
-      const formatData = JSON.parse(data);
-      const newObj = {
-        id: randomId(27),
-        name,
-        email,
-        phone,
-      };
-      return console.log(
-        (name && email && phone) !== undefined
-          ? newObj
-          : 'Incomplete information. Please enter the following data: name, email, phone!'
-      );
-    })
-    .catch(err => console.log(err));
-}
+const getContactById = async contactId => {
+  const contacts = await listContacts();
+  const result = await contacts.find(contact => contact.id === contactId);
+  return result || null;
+};
+
+const removeContact = async contactId => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex(item => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+const addContact = async data => {
+  const contacts = await listContacts();
+  const newObj = {
+    id: randomId(27),
+    ...data,
+  };
+
+  contacts.push(newObj);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newObj;
+};
 
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -5,7 +5,37 @@ const {
   addContact,
 } = require('./contacts.js');
 
-listContacts();
-getContactById('e6ywwRe4jcqxXfCZOj_1e');
-removeContact('rsKkOQUi80UsgVPCcLZZW');
-addContact('Bob Gray', 'BobGray_27dun@gmail.com', '(048) 478-3868');
+const argv = require('yargs').argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const listContact = await listContacts();
+      return console.log(listContact);
+
+    case 'get':
+      const getContact = await getContactById(id);
+      return console.log(getContact);
+
+    case 'add':
+      const newContact = await addContact({ name, email, phone });
+      return console.log(newContact);
+
+    case 'remove':
+      const deleteContact = await removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+invokeAction({ action: 'list' });
+invokeAction({ action: 'get', id: 'rsKkOQUi80UsgVPCcLZZW' });
+invokeAction({
+  action: 'add',
+  name: 'Nika Livington',
+  email: 'wejf_27dun@gmail.com',
+  phone: '(048) 478-3868',
+});
+invokeAction({ action: 'remove', id: 'ngb0tagibzm' });
